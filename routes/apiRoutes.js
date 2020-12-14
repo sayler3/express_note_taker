9800e12b625ade84518345dd6161a22862b57539
Sam Ayler <sayler8701@gmail.com>
2020-12-13 23:22:59 -0800

    adding post request for apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,11 +3,31 @@ const noteData = path.join(__dirname, "../db/db.json");
 const fs = require("fs");
 
 module.exports = (app) => {
+
     app.get('/api', (req, res) => {
         console.log("This was hit");
         fs.readFile(noteData, 'utf8', (err, data) => {
             if (err) throw err;
             res.json(JSON.parse(data));
         })
+    });
+
+    app.post('/api', (req, res) => {
+        console.log(req.body);
+        fs.readFile(noteData, 'utf8', (err, data) => {
+            if (err) throw err;
+
+            const newRes = JSON.parse(data);
+
+            newRes.push({
+                title: req.body.title,
+                text: req.body.text,
+            });
+
+            fs.writeFile(noteData, JSON.stringify(newRes), (err) => {
+                if (err) throw err;
+                res.json({ msg: 'succefully added'});
+            });
+        });
     });
 }
